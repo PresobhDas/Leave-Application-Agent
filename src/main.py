fbc6866e32c424e82ed5f7cbde81ab98c84c5492
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-12 11:39:20 -0600

    First Commit
    First Commit

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import requests
+import fastapi
+import azure.functions as func
+
+api_server = fastapi()
+
+@api_server.get('/get_weather')
+def get_weather(city='Dallas'):
+
+    def get_lat_long(city:str):
+        url = "https://geocoding-api.open-meteo.com/v1/search"
+        params = {
+            'name':city,
+            'count':1,
+            'language':'en',
+            'format':'json'
+        }
+
+        resp = requests.get(url=url, params=params)
+        data = resp.json()
+        if 'results' not in data:
+            raise ValueError(f'City:{city} not found')
+        
+        result = data['results'][0]
+        return result['latitude'], result['longitude']
+    
+    lat, long = get_lat_long(city)
+    if lat and long:
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            'latitude':lat,
+            'longitude':long,
+            'current_weather':True
+        }
+
+        resp = requests.get(url=url, params=params)
+        return resp.json()
+    
+app = func.AsgiFunctionApp(
+    app=api_server,
+    http_auth_level=func.AuthLevel.ANONYMOUS,  # or FUNCTION
+)
