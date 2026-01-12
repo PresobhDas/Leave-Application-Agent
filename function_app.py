6b4bea7293ad576f260c79a2a41207d9bfe92f3f
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-12 16:48:31 -0600

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -8,7 +8,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def ping(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("pong", status_code=200)
 
-@app.route(route="{*path}", methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    from src.api_app import api_server
-    return AsgiMiddleware(api_server).handle(req, context)
+# @app.route(route="{*path}", methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
+# def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+#     from src.api_app import api_server
+#     return AsgiMiddleware(api_server).handle(req, context)
