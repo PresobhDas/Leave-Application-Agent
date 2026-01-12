acb14cc01a4496d9209d6936bb21838b81e18197
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-12 15:31:36 -0600

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,10 +1,10 @@
 import azure.functions as func
 from azure.functions import AsgiMiddleware
-from src.api_app import api_app as fastapi_app
+from src.api_app import api_server
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.function_name(name="FastAPI")
 @app.route(route="{*route}", methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return AsgiMiddleware(fastapi_app).handle(req, context)
+    return AsgiMiddleware(api_server).handle(req, context)
