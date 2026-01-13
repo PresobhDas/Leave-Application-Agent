9e3ddba7237cb1ecb82bbb0a60fd41400a355b31
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-12 20:52:50 -0600

    Update api_app.py

--- src/api_app.py
+++ src/api_app.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 
 api_server = FastAPI()
 
-@api_server.get("/ping")
+@api_server.get("ping")
 async def ping():
     return {"message": "pong"}
 
-@api_server.get('/get_weather')
+@api_server.get('get_weather')
 async def get_weather():
     return {'city': 'Dallas'}
 
