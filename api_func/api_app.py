b9b3ed4e0e9aa11e9ee58e17a43a7d48f3b1f893
PSobhana_sbhinc <PSobhana+sbhinc@Sallybeauty.com>
2026-01-14 14:09:48 -0600

    commit

--- api_func/api_app.py
+++ api_func/api_app.py
@@ -1,13 +1,27 @@
 # import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+import os
 
 api_server = FastAPI()
+class InputDetails(BaseModel):
+    inp_query:str
 
-@api_server.get("/ping")
+@api_server.get('/ping')
 async def ping():
-    return {"message": "pong"}
+    return {'response':'pong'}
 
-@api_server.get('/get_weather')
-async def get_weather():
-    return {'city': 'Dallas'}
+@api_server.get('/agent')
+async def call_agent(inp_details : InputDetails):
+    chat_model = ChatOpenAI(
+        name='gpt-4o-mini',
+        api_key=os.environ['OPENAI_API_KEY']
+    )
+    inp_query = inp_details.inp_query
+
+    response = chat_model.invoke(inp_query)
+
+    return response
+
 
---
+++ main_agent.py
@@ -0,0 +1,5 @@
+import requests
+
+url = 'https://leave-policy-agent-aaavdzbuf3bcexej.westus2-01.azurewebsites.net/ping'
+
+response = requests.get(url=url)
