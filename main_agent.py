443eb9f4be67811315f95b0e00273b3bccc453bc
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-15 13:13:00 -0600

    Update main_agent.py

--- main_agent.py
+++ main_agent.py
@@ -8,4 +8,4 @@ url = 'https://leave-policy-agent-aaavdzbuf3bcexej.westus2-01.azurewebsites.net/
 inp_details = InputDetails(inp_query='Hello How are you')
 response = requests.get(url=url, json=inp_details.model_dump())
 
-print(response.json()['content'])
+print(response)
