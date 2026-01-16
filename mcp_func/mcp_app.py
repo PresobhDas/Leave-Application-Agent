156b859811b241ff156f48a3e21d2465ca0441a4
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-16 15:29:16 -0600

    Update mcp_app.py

--- mcp_func/mcp_app.py
+++ mcp_func/mcp_app.py
@@ -3,14 +3,17 @@ import requests
 from mcp.server.transport_security import TransportSecuritySettings
 import logging, sys
 
-log = logging.getLogger()
+log = logging.getLogger("api")
 log.setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.INFO) 
 log.propagate = True
-
 if not log.handlers:
     h = logging.StreamHandler(sys.stdout)
     h.setLevel(logging.INFO)
     log.addHandler(h)
+
+log.info("mcp_app.py logging is ON")
+print("mcp_app.py PRINT sanity check")
 
 HOST = 'leave-policy-agent-mcp-aseufdafbndad6a8.westus2-01.azurewebsites.net'
 
@@ -27,7 +30,7 @@ mcp_api_app = FastMCP(
 @mcp_api_app.tool()
 async def get_weather(city:str):
     log.info('Successfully got into the MCP for get_weather')
-    print('Printing from the print function')
+    print('Printing from the print')
     return {'Weather of Dallas' : 'Sunny'}
     # def get_lat_long(city:str):
     #     url = "https://geocoding-api.open-meteo.com/v1/search"
