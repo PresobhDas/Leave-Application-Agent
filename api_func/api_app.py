cabeee34702330db3ad07d46f7201876c585cf0a
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-16 11:56:27 -0600

    Update api_app.py

--- api_func/api_app.py
+++ api_func/api_app.py
@@ -26,6 +26,7 @@ async def call_agent(inp_details : Annotated[InputDetails, Body()]):
         async with streamable_http_client('https://leave-policy-agent-mcp-aseufdafbndad6a8.westus2-01.azurewebsites.net/mcp') as (read, write):
             async with ClientSession(read, write) as session:
                 await session.initialize()
+                log.info('MCP session initialized')
                 log.info(f'Data from MCP is {await session.call_tool(name='get_weather',arguments={'city':'Dallas'})}')
     except* Exception as e:
         log.exception(f'Failed with Exception: {e.exceptions}')
