f665e744a20a9313debb7d65f344c853561770be
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-01-16 14:35:24 -0600

    Update mcp_app.py

--- mcp_func/mcp_app.py
+++ mcp_func/mcp_app.py
@@ -23,34 +23,35 @@ mcp_api_app = FastMCP(
 @mcp_api_app.tool()
 async def get_weather(city:str):
     log.info('Successfully got into the MCP for get_weather')
-    def get_lat_long(city:str):
-        url = "https://geocoding-api.open-meteo.com/v1/search"
-        params = {
-            'name':city,
-            'count':1,
-            'language':'en',
-            'format':'json'
-        }
+    return {'Weather of Dallas' : 'Sunny'}
+    # def get_lat_long(city:str):
+    #     url = "https://geocoding-api.open-meteo.com/v1/search"
+    #     params = {
+    #         'name':city,
+    #         'count':1,
+    #         'language':'en',
+    #         'format':'json'
+    #     }
 
-        resp = requests.get(url=url, params=params)
-        data = resp.json()
-        if 'results' not in data:
-            raise ValueError(f'City:{city} not found')
+    #     resp = requests.get(url=url, params=params)
+    #     data = resp.json()
+    #     if 'results' not in data:
+    #         raise ValueError(f'City:{city} not found')
         
-        result = data['results'][0]
-        return result['latitude'], result['longitude']
+    #     result = data['results'][0]
+    #     return result['latitude'], result['longitude']
     
-    lat, long = get_lat_long(city)
-    if lat and long:
-        url = "https://api.open-meteo.com/v1/forecast"
-        params = {
-            'latitude':lat,
-            'longitude':long,
-            'current_weather':True
-        }
+    # lat, long = get_lat_long(city)
+    # if lat and long:
+    #     url = "https://api.open-meteo.com/v1/forecast"
+    #     params = {
+    #         'latitude':lat,
+    #         'longitude':long,
+    #         'current_weather':True
+    #     }
 
-        resp = requests.get(url=url, params=params)
-        log.info('get_weather API successfully called.')
-        return resp.json()
+    #     resp = requests.get(url=url, params=params)
+    #     log.info('get_weather API successfully called.')
+    #     return resp.json()
     
 mcp_server = mcp_api_app.streamable_http_app()
