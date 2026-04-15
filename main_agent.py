fcb83ae53bfe7c24cc92f5008bddfda5642c8d8c
presobh_personal <presobhdas@gmail.com>
2026-04-15 13:44:41 -0500

    commit

--- main_agent.py
+++ main_agent.py
@@ -4,19 +4,18 @@ from pydantic import BaseModel
 class InputDetails(BaseModel):
     inp_query:str
 
-url = 'https://leave-agent-api.ashyglacier-369787e5.westus2.azurecontainerapps.io/ingest'
+url = 'https://leave-agent-api.ashyglacier-369787e5.westus2.azurecontainerapps.io/agent'
 # leave-policy-agent-mcp-1-auczcxdxa7dwftd9.westus2-01.azurewebsites.net
 
-inp_details = InputDetails(inp_query='What is the weather in Greensboro')
+inp_details = InputDetails(inp_query='Get me more information about Pay for Performance')
 response = requests.post(url=url, json=inp_details.model_dump())
 # response = requests.post(url=url)
-print(response.json())
 # response = requests.get(url=url, timeout=20)
 
 
-# data = response.json()
+data = response.json()
 
-# for contents in data['messages']:
-#     print(contents['type'])
-#     print('-'*5)
-#     print(contents['content'])
+for contents in data['messages']:
+    print(contents['type'])
+    print('-'*5)
+    print(contents['content'])
