12e4245a9eaa359491f19ff50dd6ab8c2ca2d847
presobh_personal <presobhdas@gmail.com>
2026-04-22 22:38:24 -0500

    Update llm_utils.py

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -371,6 +371,7 @@ def extract_rag_data(state):
 
     if rag_msgs:
         try:
+            log.info(f'CUSTOM LOG . raw data before parsing is {rag_msgs[-1]}')
             parsed = RagDataResponseModel.model_validate_json(rag_msgs[-1].content)
             log.info(f'CUSTOM LOG . Data in the extract_rag is {parsed}')
 
