22cbe131d24783b5f651696b24a803fc91b08d09
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-05-01 12:41:29 -0500

    Update api_app.py

--- api_func/api_app.py
+++ api_func/api_app.py
@@ -124,7 +124,7 @@ async def ingest_pipeline(request:Request):
             log.info(f'CUSTOM - LOG : Name of the file name is {file_name}')
             result = poller.result().as_dict()
             json_blob_client = blob_service_client.get_blob_client(
-                container='rag_docs_json',
+                container='rag-docs-json',
                 blob=f'{file_name}.json'
             )
 
