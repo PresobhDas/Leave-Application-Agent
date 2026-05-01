60fb27b79b8a172258a965a631d1c86a51232c86
PresobhDas <97720741+PresobhDas@users.noreply.github.com>
2026-05-01 12:27:40 -0500

    Update api_app.py

--- api_func/api_app.py
+++ api_func/api_app.py
@@ -120,6 +120,7 @@ async def ingest_pipeline(request:Request):
                 model_id='prebuilt-layout',
                 body=pdf_bytes
             )
+            log.info(f'CUSTOM - LOG : Name of the blob name is {blob_name}')
             result = poller.result().as_dict()
             json_blob_client = blob_service_client.get_blob_client(
                 container='rag_docs_json',
